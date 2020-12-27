c9106d2035ef4d32ba2becbcff859d19b6f64e2d
Evan Bacon <evan@baconblvd.com>
2020-12-26 19:01:19 -0800

    Initial Commit

---
+++ Calendex/CalendexApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalendexApp.swift
+//  Calendex
+//
+//  Created by Evan Bacon on 12/26/20.
+//
+
+import SwiftUI
+
+@main
+struct CalendexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
